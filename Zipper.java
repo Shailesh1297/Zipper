1046f4f96e59d8dc7bb4d74dce3cebe61e2f1162
Shailesh Kumar <38895259+Shailesh1297@users.noreply.github.com>
2019-03-15 21:36:16 +0530

    Add files via upload
    Zipper(main)

---
+++ Zipper.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zipper;
+
+/**
+ *
+ * @author UCHIHA
+ */
+public class Zipper {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Console cns=new Console();
+        cns.setTitle("Zipper_v0.0");
+        cns.setVisible(true);
+        cns.setSize(380,160);
+        cns.setResizable(false);
+        cns.setDefaultCloseOperation(Console.EXIT_ON_CLOSE);
+    }
+    
+}
